e07eab1ece090aefb1767938fbc80fb78f1d4541
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-07-31 22:14:25 +0300

    feat: finish react components task

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,19 +6,47 @@ import cards from '../../modules/cards';
 import Card from '../card/card';
 import SearchBar from '../searchBar/searchBar';
 
-function App() {
-  return (
-    <div className="main">
-      <h1 className="logo">React components task</h1>
-      <SearchBar />
-      <div className="cards__field">
-        {cards.map((card) => {
-            <Card key={card.id} card={card} />
-          })
-        }
+class App extends React.Component {
+
+  state = {
+    cardIsLike: [false, false],
+  }
+
+  handleLikedState = (index: number) => {
+    console.log(`ДО: ${this.state.cardIsLike}`);
+
+    const arr = this.state.cardIsLike;
+    arr[index] = true;
+    console.log(arr);
+
+    
+    this.setState({
+      cardIsLike: arr,
+    })
+
+    console.log(`После: ${this.state.cardIsLike}`);
+    
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1 className="logo">React components task</h1>
+        <SearchBar />
+        <div className="cards__field">
+          {cards.map((card) => {
+             return <Card 
+                      key={card.id} 
+                      card={card}
+                      isLiked={this.state.cardIsLike[card.id - 1]}
+                      onHandleLiked={this.handleLikedState.bind(this, (card.id - 1))}
+                      />
+            })
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/modules/interfaces.ts
+++ src/modules/interfaces.ts
@@ -1,12 +1,10 @@
 export default interface CardObj {
-  card: {
-    id: number,
-    link: string,
-    imgURL: string,
-    name: string,
-    subtitle: string,
-    adressLink: string,
-    adress: string,
-    date: string,
-  }
+  id: number,
+  link: string,
+  imgURL: string,
+  name: string,
+  subtitle: string,
+  adressLink: string,
+  adress: string,
+  date: string,
 }
