920581816e174d09250a77278face3d88d86a47d
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-08-06 10:38:20 +0300

    feat: inputs was added

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,23 +4,34 @@ import React from 'react';
 import Header from '../header/header';
 import Form from '../form/form';
 
+  export interface InputValues {
+    firstName: string,
+    lastName: string,
+    email: string,
+    birthdayDate: string,
+    country: string,
+    offer: boolean,
+    agree: boolean,
+  }
 
 class App extends React.Component {
-
   state: {
-    formActive: string,
-    name: boolean,
-  }
+    formActive: string;
+    formValues: {
+
+    }
+  };
 
   constructor(props: {} | Readonly<{}>) {
     super(props);
 
     this.state = {
       formActive: 'none',
-      name: false,
-    }
+      formValues: {
 
-  };
+      }
+    };
+  }
 
   activateInputForm = () => {
     this.setState({
@@ -32,28 +43,30 @@ class App extends React.Component {
     if (className === 'app__field active') {
       this.setState({
         formActive: 'none',
-      })
+      });
     }
   };
+
+  setFormValues = (values: InputValues): void => {
+    console.log(values);
+  }
 
   render() {
-    let form: JSX.Element | null = <Form />
+    let form: JSX.Element | null = <Form setFormValues={this.setFormValues}/>;
     if (this.state.formActive === 'none') {
-      form = null
+      form = null;
     }
     return (
-      <div className='wrapper'>
-        <div 
+      <div className="wrapper">
+        <div
           className={`app__field ${this.state.formActive}`}
-          onClick={(e) => this.closeInputForm(e.target.className)}
-        >
+          onClick={(e) => this.closeInputForm(e.target.className)}>
           <Header activateForm={this.activateInputForm} />
         </div>
-        { form }
-      </div> 
-    )
-  };
-
-};
+        {form}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,17 +1,171 @@
 import './form.scss';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { InputValues } from '../app/App';
 
-const Form = () => {
+const Form = (setFormValues: (values: InputValues) => void) => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthdayDate, setBirthdayDate] = useState('');
+    const [country, setCountry] = useState('Belarus');
+    const [offer, setOffer] = useState(false);
+    const [agree, setAgree] = useState(false);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setFormValues({
+            firstName,
+            lastName,
+            email,
+            birthdayDate,
+            country,
+            offer,
+            agree
+        });
+    };
+
+    const  handleInput = (event: string, func: { (value: React.SetStateAction<string>): void; (arg0: string): void; }) => {
+        func(event);
+    }
+
     return (
         <div className='app__form'>
-            <form name='reg__form'>
-                <input type='text'></input>
-                <input type='text'></input>
-                <input type='email'></input>
-                <input type='tel'></input>
-                <input type='date'></input>
-                <input type='file'></input>
+            <form 
+                className='reg__form'
+                onSubmit={(event) => handleSubmit(event)}
+                name='reg-form'
+            >
+                <label 
+                    className='form__label'
+                    htmlFor='firstName'
+                >
+                  Name:
+                    <input
+                        type='text' 
+                        className='form__input'
+                        required
+                        placeholder='example: Alex'
+                        name='firstName'
+                        value={firstName}
+                        onChange={(event) => handleInput(event.target.value, setFirstName)}
+                    />
+                </label>
+                <label 
+                    className='form__label'
+                    htmlFor='lastName'
+                >
+                  Last Name:
+                    <input
+                        type='text'
+                        className='form__input'
+                        required
+                        placeholder='example: Novorski'
+                        name='lastName'
+                        value={lastName}
+                        onChange={(event) => handleInput(event.target.value, setLastName)}
+                    />
+                </label>
+                <label 
+                    className='form__label'
+                    htmlFor='Email'
+                >
+                  Email:
+                    <input
+                        type='text'
+                        className='form__input'
+                        required
+                        placeholder='example: alex@example.com'
+                        name='email'
+                        value={email}
+                        onChange={(event) => handleInput(event.target.value, setEmail)}
+                    />
+                </label>
+                <label
+                    className='form__label'
+                    htmlFor='birthdayDate'
+                >
+                  Your`s birthday:
+                    <input
+                        type='date'
+                        className='form__input date'
+                        required
+                        name='birthdayDate'
+                        value={birthdayDate}
+                        onChange={(event) => handleInput(event.target.value, setBirthdayDate)}
+                    />
+                </label>
+                <label
+                    className='form__label'
+                    htmlFor='country'
+                >
+                  Country:
+                    <select
+                        className='form__input'
+                        required
+                        name='country'
+                        value={country}
+                        onChange={(event) => handleInput(event.target.value, setCountry)}
+                    >
+                        <option>Russia</option>
+                        <option>Belarus</option>
+                        <option>USA</option>
+                        <option>Franch</option>
+                        <option>Germany</option>
+                    </select>
+                </label>
+                <label
+                    className='form__label offer'
+                    htmlFor='offer'
+                >
+                  I agree to create a card:
+                    <input
+                        type='checkbox'
+                        className='form__input'
+                        required
+                        name='offer'
+                        checked={offer}
+                        style={{margin: '0 0 10px 10px'}}
+                        onChange={() => setOffer(prev => !prev)}
+                    />
+                </label>
+                <label
+                    className='form__label'
+                    htmlFor='agree'
+                >
+                  I agree to create a card:
+                <div className="onoffswitch">
+                    <input 
+                        type="checkbox" 
+                        name="onoffswitch" 
+                        className="onoffswitch-checkbox" 
+                        id="myonoffswitch"
+                        checked={agree}
+                        onChange={() => setAgree(prev => !prev)}
+                    />
+                    <label 
+                        className="onoffswitch-label" 
+                        htmlFor="myonoffswitch"
+                    />
+                </div>
+                </label>
+                <label
+                    className='form__label'
+                    htmlFor='avatar'
+                >
+                  Upload avatar:
+                    <input
+                        className='form__input'
+                        type='file'
+                        name='avatar'
+                    />
+                </label>
+                <input
+                    className='form__btn'
+                    type='submit'
+                    value='Create'
+                />
             </form>
         </div>
     )
