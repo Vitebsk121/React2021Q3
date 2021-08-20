617afe1d0607ec3b3b536e4d1bad72d3491b8ffe
Vitebsk121 <Alexandr.demchenko@list.ru>
2021-08-20 15:03:42 +0300

    feat: add ErrorPage to rout

--- src/containers/HomePage/Header/Header.tsx
+++ src/containers/HomePage/Header/Header.tsx
@@ -1,19 +1,21 @@
 import './Header.scss';
 
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 import SearchForm from '../../../components/SearchForm/SearchForm';
+import { IHeaderProps } from '../../../shared/interfaces';
 
-const Header = () => (
-  <div className="Header">
-    <a className="Logo" href="./">
-      <h1>React Routing</h1>
-    </a>
-    <SearchForm />
-    <a href="https://newsapi.org/" target="_blank" rel="noreferrer">
-      <b>News API</b>
-    </a>
-  </div>
-);
-
-export default Header;
+export default function Header({ isLoading, onSubmit }: IHeaderProps) {
+  return (
+    <div className="Header">
+      <NavLink className="Logo" to="/home">
+        <h1>React Routing</h1>
+      </NavLink>
+      <SearchForm isLoading={isLoading} onSubmit={onSubmit} />
+      <a href="https://newsapi.org/" target="_blank" rel="noreferrer">
+        <b>News API</b>
+      </a>
+    </div>
+  );
+}
